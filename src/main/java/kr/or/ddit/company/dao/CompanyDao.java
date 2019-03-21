4f6d2b991496ec0fa7d25489e87bf0e2cf552fd4
jhlee <jhlee.lubobo128@gmail.com>
2019-03-21 17:53:58 +0900

    타일 부분 사이드바 적용

---
+++ src/main/java/kr/or/ddit/company/dao/CompanyDao.java
@@ -0,0 +1,42 @@
+package kr.or.ddit.company.dao;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.stereotype.Repository;
+
+import kr.or.ddit.company.model.CompanyVo;
+
+@Repository("companyDao")
+public class CompanyDao implements ICompanyDao{
+	
+	@Resource(name="sqlSessionTemplate")
+    private SqlSessionTemplate sqlSessionTemplate;
+
+	@Override
+	public List<CompanyVo> getAllcompany() {
+		List<CompanyVo> selectList = sqlSessionTemplate.selectList("company.getAllcompany");
+		return selectList;
+	}
+
+	@Override
+	public CompanyVo selectCompany(String companycode) {
+		CompanyVo selectOne = sqlSessionTemplate.selectOne("company.selectCompany",companycode);
+		return selectOne;
+	}
+
+	@Override
+	public int insertCompany(CompanyVo companyVo) {
+		int insert = sqlSessionTemplate.insert("company.insertCompany");
+		return insert;
+	}
+
+	@Override
+	public int deleteCompany(String companycode) {
+		int delete = sqlSessionTemplate.delete("company.deleteCompany");
+		return delete;
+	}
+
+}

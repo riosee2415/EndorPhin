270613d3a29fcd9280d50fd411cc4d153daeac11
강현욱 <thy1348@gmail.com>
2019-03-25 11:29:53 +0900

    boardType service 추

---
+++ src/main/java/kr/or/ddit/board/service/Board_TypeService.java
@@ -0,0 +1,78 @@
+package kr.or.ddit.board.service;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+import kr.or.ddit.board.dao.IBoard_TypeDao;
+import kr.or.ddit.board.model.Board_TypeVo;
+
+@Service("board_TypeService")
+public class Board_TypeService implements IBoard_TypeService{
+
+	@Resource(name="board_TypeDao")
+	private IBoard_TypeDao board_TypeDao;
+
+	/**
+	 * 
+	* Method : boardAllList
+	* 작성자 : macbook
+	* 변경이력 :
+	* @return
+	* Method 설명 : 게시판 조회
+	 */
+	@Override
+	public List<Board_TypeVo> boardAllList() {
+		
+		List<Board_TypeVo> boardAllList = board_TypeDao.boardAllList();
+		return boardAllList;
+	}
+
+	/**
+	 * 
+	* Method : insertBoard
+	* 작성자 : macbook
+	* 변경이력 :
+	* @param board_TypeVo
+	* @return
+	* Method 설명 : 게시판 생성
+	 */
+	@Override
+	public int insertBoard(Board_TypeVo board_TypeVo) {
+		
+		int insertBoard = board_TypeDao.insertBoard(board_TypeVo);
+		return insertBoard;
+	}
+
+	/**
+	 * 
+	* Method : updateBoard
+	* 작성자 : macbook
+	* 변경이력 :
+	* @param board_TypeVo
+	* @return
+	* Method 설명 : 게시판 수정
+	 */
+	@Override
+	public int updateBoard(Board_TypeVo board_TypeVo) {
+		int updateBoard = board_TypeDao.updateBoard(board_TypeVo);
+		return updateBoard;
+	}
+
+	/**
+	 * 
+	* Method : deleteBoard
+	* 작성자 : macbook
+	* 변경이력 :
+	* @param boardTypeCode
+	* @return
+	* Method 설명 : 게시판 삭제
+	 */
+	@Override
+	public int deleteBoard(String boardTypeCode) {
+		int deleteBoard = board_TypeDao.deleteBoard(boardTypeCode);
+		return deleteBoard;
+	}
+}
---
+++ src/main/java/kr/or/ddit/board/service/IBoard_TypeService.java
@@ -0,0 +1,51 @@
+package kr.or.ddit.board.service;
+
+import java.util.List;
+
+import kr.or.ddit.board.model.Board_TypeVo;
+
+public interface IBoard_TypeService {
+	
+	/**
+	 * 
+	* Method : boardAllList
+	* 작성자 : macbook
+	* 변경이력 :
+	* @return
+	* Method 설명 : 전체 게시판 전체 조회
+	 */
+	List<Board_TypeVo> boardAllList();
+	
+	/**
+	 * 
+	* Method : insertBoard
+	* 작성자 : macbook
+	* 변경이력 :
+	* @param board_TypeVo
+	* @return
+	* Method 설명 : 게시판 생성
+	 */
+	int insertBoard(Board_TypeVo board_TypeVo);
+	
+	/**
+	 * 
+	* Method : updateBoard
+	* 작성자 : macbook
+	* 변경이력 :
+	* @return
+	* Method 설명 : 게시판 수정
+	 */
+	int updateBoard(Board_TypeVo board_TypeVo);
+	
+	/**
+	 * 
+	* Method : deleteBoard
+	* 작성자 : macbook
+	* 변경이력 :
+	* @param boardTypeCode
+	* @return
+	* Method 설명 : 게시판 삭제
+	 */
+	int deleteBoard(String boardTypeCode);
+
+}

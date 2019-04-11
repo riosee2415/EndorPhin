0e4c2d68153adaee07f1ac38f4327a76985b01ff
jhlee <jhlee.lubobo128@gmail.com>
2019-04-11 09:50:23 +0900

    에러 수정 ;

--- src/test/java/kr/or/ddit/images/service/ImagesServiceTest.java
+++ src/test/java/kr/or/ddit/images/service/ImagesServiceTest.java
@@ -13,6 +13,7 @@ import org.slf4j.LoggerFactory;
 
 import kr.or.ddit.image.dao.IImagesDao;
 import kr.or.ddit.image.model.ImagesVo;
+import kr.or.ddit.image.service.IImagesService;
 import kr.or.ddit.product.model.ProductVo;
 import kr.or.ddit.set.LogicConfig;
 
@@ -20,31 +21,25 @@ public class ImagesServiceTest extends LogicConfig{
 
 	private Logger logger = LoggerFactory.getLogger(ImagesServiceTest.class);
 	
-	@Resource(name="imagesDao")
-	IImagesDao imagesDao;
+	@Resource(name="imagesService")
+	IImagesService imagesService;
 	
 	@Test
 	public void getAllImagesTest(){
-		List<ImagesVo> allImages = imagesDao.getAllImages();
+		List<ImagesVo> allImages = imagesService.getAllImages();
 		logger.debug("asdfljnsadf:{}",allImages.size());
 	}
 	
 	@Test
-	public void insertImagesTest() {
-		ImagesVo imagesVo= new ImagesVo("1","c:/","haha.png");
-		int insertPayment = imagesDao.insertImages(imagesVo);
-		logger.debug("확인 : {}",imagesVo.getImageCode());
-		assertEquals(1, insertPayment);
-	}
-	@Test
 	public void selectImagesTest() {
-		ImagesVo selectImages = imagesDao.selectImages("1");
+		ImagesVo selectImages = imagesService.selectImages("1");
 		assertTrue(selectImages.getImageName().equals("자"));
 	}
 	@Test
 	public void deleteImagesTest() {
-		int deleteProduct = imagesDao.deleteImages("2");
+		int deleteProduct = imagesService.deleteImages("2");
 		assertEquals(1, deleteProduct);
 	}
-
 }
+
+
